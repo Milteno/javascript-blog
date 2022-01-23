9a39a817dee48b1101ed8cbaacfe635525f38d70
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-23 17:10:35 +0100

    Ex5-full in script.js

--- js/script.js
+++ js/script.js
@@ -32,18 +32,60 @@ const titleClickHandler = function(event){
 
     /* get 'href' attribute from the clicked link */
     const hrefAttrib = clickedElement.getAttribute('href');
-    console.log(hrefAttrib);
 
     /* find the correct article using the selector (value of 'href' attribute) */
     const findArticle = document.querySelector(hrefAttrib);
-    console.log(findArticle);
 
     /* add class 'active' to the correct article */
     findArticle.classList.add('active');
   }
 
-  const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+        /* remove contents of titleList */
+        const listTitles = document.querySelector('.titles');
+        const linksContent = document.querySelectorAll('.titles span');
+        let html = '';
+
+        for(let span of linksContent){
+          span.innerHTML=" ";
+        }
+
+        /* for each article */
+        const articles = document.querySelectorAll(optArticleSelector);
+
+        for(let article of articles){
+            const articleId = article.getAttribute('id');
+            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+            //console.log(linkHTML);
+            //listTitles.insertAdjacentHTML("beforebegin", linkHTML);
+            html = html + linkHTML;
+        }
+        listTitles.innerHTML = html;
+        /* get the article id */
+
+
+        /* find the title element */
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+        const links = document.querySelectorAll('.titles a');
+        console.log(links);
+
+        for(let link of links){
+            link.addEventListener('click', titleClickHandler);
+        }
+        }
+    generateTitleLinks();
+
+
