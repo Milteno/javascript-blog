2222333c47ab303ae5d7a476eb9eebad3ea543d0
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-01-23 18:03:29 +0100

    Install ESLint

--- js/script.js
+++ js/script.js
@@ -38,54 +38,53 @@ const titleClickHandler = function(event){
 
     /* add class 'active' to the correct article */
     findArticle.classList.add('active');
-  }
+};
 
 
 
-    const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title';
 
-    function generateTitleLinks(){
+function generateTitleLinks(){
 
-        /* remove contents of titleList */
-        const listTitles = document.querySelector('.titles');
-        const linksContent = document.querySelectorAll('.titles span');
-        let html = '';
+    /* remove contents of titleList */
+    const listTitles = document.querySelector('.titles');
+    const linksContent = document.querySelectorAll('.titles span');
+    let html = '';
 
-        for(let span of linksContent){
-          span.innerHTML=" ";
-        }
+    for(let span of linksContent){
+        span.innerHTML=' ';
+    }
 
-        /* for each article */
-        const articles = document.querySelectorAll(optArticleSelector);
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
 
-        for(let article of articles){
-            const articleId = article.getAttribute('id');
-            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-            //console.log(linkHTML);
-            //listTitles.insertAdjacentHTML("beforebegin", linkHTML);
-            html = html + linkHTML;
-        }
-        listTitles.innerHTML = html;
-        /* get the article id */
+    for(let article of articles){
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        //console.log(linkHTML);
+        //listTitles.insertAdjacentHTML("beforebegin", linkHTML);
+        html = html + linkHTML;
+    }
+    listTitles.innerHTML = html;
+    /* get the article id */
 
 
-        /* find the title element */
+    /* find the title element */
 
-        /* get the title from the title element */
+    /* get the title from the title element */
 
-        /* create HTML of the link */
+    /* create HTML of the link */
 
-        /* insert link into titleList */
-        const links = document.querySelectorAll('.titles a');
-        console.log(links);
+    /* insert link into titleList */
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
 
-        for(let link of links){
-            link.addEventListener('click', titleClickHandler);
-        }
-        }
-    generateTitleLinks();
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }
+}
+generateTitleLinks();
 
 
